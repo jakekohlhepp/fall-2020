340f5f63d25e21b47ff0b41a528e91a479f308bc
jacob kohlhepp <jakekohlhepp@gmail.com>
2020-10-05 14:16:39 -0700

    begin ps6.

---
+++ ProblemSets/PS6-ccp/ps6_sol.jl
@@ -0,0 +1,38 @@
+using Random
+using LinearAlgebra
+using Statistics
+using Optim
+using DataFrames
+using DataFramesMeta
+using CSV
+using HTTP
+using GLM
+
+
+# 1. read in and reshape
+cd("C:/Users/jakek/Documents/econ_phd/structural_modeling_course/forked_class/ProblemSets/PS5-ddc/")
+include("create_grids.jl")
+
+# transform to long.
+df = CSV.read("busdata.csv")
+df = @transform(df, bus_id = 1:size(df,1))
+dfy = @select(df, :bus_id,:Y1,:Y2,:Y3,:Y4,:Y5,:Y6,:Y7,:Y8,:Y9,:Y10,:Y11,:Y12,:Y13,:Y14,:Y15,:Y16,:Y17,:Y18,:Y19,:Y20,:RouteUsage,:Branded)
+dfy_long = DataFrames.stack(dfy, Not([:bus_id,:RouteUsage,:Branded]))
+rename!(dfy_long, :value => :Y)
+dfy_long = @transform(dfy_long, time = kron(collect([1:20]...),ones(size(df,1))))
+select!(dfy_long, Not(:variable))
+
+# next reshape the odometer variable
+dfx = @select(df, :bus_id,:Odo1,:Odo2,:Odo3,:Odo4,:Odo5,:Odo6,:Odo7,:Odo8,:Odo9,:Odo10,:Odo11,:Odo12,:Odo13,:Odo14,:Odo15,:Odo16,:Odo17,:Odo18,:Odo19,:Odo20)
+dfx_long = DataFrames.stack(dfx, Not([:bus_id]))
+rename!(dfx_long, :value => :Odometer)
+dfx_long = @transform(dfx_long, time = kron(collect([1:20]...),ones(size(df,1))))
+select!(dfx_long, Not(:variable))
+
+# join reshaped df's back together
+df_long = leftjoin(dfy_long, dfx_long, on = [:bus_id,:time])
+sort!(df_long,[:bus_id,:time])
+
+## 2. Run flexible logit
+beta_glm = glm(@formula(Y ~ Odometer*Odometer^2*RouteUsage*RouteUsage^2*Branded*time*time^2 ), df_long, Binomial(), LogitLink())
+println(beta_glm)
